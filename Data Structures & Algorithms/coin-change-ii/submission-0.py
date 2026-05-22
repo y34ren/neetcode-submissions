131d06b5b604a90047901035c9b46a076778cf8c
y34ren <39224162+y34ren@users.noreply.github.com>
2026-05-21 22:45:30 -0400

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+         # dp[x] =
+        # number of ways to make amount x
+        dp = [0] * (amount + 1)
+
+        # Base case:
+        # There is exactly one way
+        # to make amount 0:
+        # choose no coins
+        dp[0] = 1
+
+        # Process each coin one at a time
+        for coin in coins:
+
+            # Update all amounts
+            # that can include this coin
+            for current_amount in range(coin, amount + 1):
+
+                # Add:
+                # ways to make the remaining amount
+                dp[current_amount] += (
+                    dp[current_amount - coin]
+                )
+
+        # Final answer
+        return dp[amount]
